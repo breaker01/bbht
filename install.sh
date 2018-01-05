b7e45c6fab079475ed3c5e941a94aaa6e1081be6
JR0ch17 <JR0ch17@users.noreply.github.com>
2018-01-05 08:53:21 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo "Libraries, Ruby, PHP, etc installed!"
 
 echo "Ready to install tools..."
 
-echo "Installing awscli..."
+echo "Installing AWSCLI..."
 pip install awscli
 echo "AWS CLI installed!"
 echo "Don't forget to set up AWS credentials using the `aws configure` command!"
@@ -35,10 +35,22 @@ sudo git clone https://github.com/sxcurity/alive-host
 sudo chmod +x alive-host/alive.sh
 echo "Alive-Host installed!"
 
-echo "Installing Tomcat-CVE-2017-12617..."
-sudo git clone https://github.com/cyberheartmi9/CVE-2017-12617
-sudo chmod +x CVE-2017-12617/tomcat-cve-2017-12617.py
-echo "Tomcat-CVE-2017-12617 installed!"
+echo "Installing BFAC..."
+sudo git clone https://github.com/mazen160/bfac
+cd bfac/
+sudo python setup.py install
+cd ../
+echo "BFAC installed!"
+
+echo "Installing C5Scan..."
+sudo pip install httplib2 requests
+sudo git clone https://github.com/auraltension/c5scan
+echo "C5Scan installed!"
+
+echo "Installing CMSFuzz..."
+sudo git clone https://github.com/nahamsec/CMSFuzz
+sudo chmod +x CMSFuzz/dirbf.rb
+echo "CMSFuzz installed!"
 
 echo "Installing Dirsearch..."
 sudo git clone https://github.com/maurosoria/dirsearch
@@ -48,6 +60,9 @@ echo "Installing DomainWatch..."
 sudo git clone https://github.com/ebelties/DomainWatch
 sudo chmod +x DomainWatch/domainwatch.sh
 echo "DomainWatch installed!"
+
+echo "Installing DroopeScan..."
+sudo pip install droopescan
 
 echo "Installing GitTools..."
 sudo git clone https://github.com/internetwache/GitTools
@@ -65,17 +80,50 @@ sudo git clone https://github.com/nahamsec/HostileSubBruteforcer
 sudo chmod +x HostileSubBruteforcer/sub_brute.rb
 echo "HostileSubBruteForcer installed!"
 
+echo "Installing JoomScan..."
+sudo git clone https://github.com/rezasp/joomscan
+echo "JoomScan installed!"
+
 echo "Installing JSParser..."
-sudo git clone https://github.com/nahamsec/JSParser.git
+sudo git clone https://github.com/nahamsec/JSParser
 sudo chmod +x JSParser/*.py
 cd JSParser
 sudo python setup.py install
-cd ~/tools/
+cd ../
 echo "JSParser installed!"
+
+echo "Installing JS-Scan..."
+sudo git clone cd https://github.com/zseano/JS-Scan
+echo "JS-Scan installed!"
 
 echo "Installing Liffy..."
 sudo git clone https://github.com/hvqzao/liffy
 sudo chmod +x liffy/*.py
+
+echo "Installing LinkFinder..."
+sudo git clone https://github.com/GerbenJavado/LinkFinder
+cd LinkFinder/
+sudo chmod +x setup.py
+sudo python setup.py install
+cd ../
+echo "LinkFinder installed!"
+
+echo "Installing Nikto..."
+sudo apt-get install libnet-ssleay-perl libwhisker2-perl openssl -y
+sudo wget https://cirt.net/nikto/nikto-2.1.5.tar.gz
+sudo tar xvfz nikto-2.1.5.tar.gz
+sudo rm nikto-2.1.5.tar.gz
+sudo chmod +x nikto-2.1.5/*.pl
+sudo perl nikto-2.1.5/nikto.pl -update
+echo "Nikto installed!"
+
+echo "Installing Nmap 7.60..."
+sudo apt-get install alien
+sudo wget https://nmap.org/dist/nmap-7.60-1.x86_64.rpm
+sudo alien nmap-7.60-1.x86_64.rpm
+sudo dpkg --install nmap_7.60-2_amd64.deb
+sudo rm nmap*
+echo "Nmap installed!"
 
 echo "Installing Parameth..."
 sudo git clone https://github.com/maK-/parameth
@@ -103,6 +151,10 @@ echo "Installing struts-pwn..."
 sudo git clone https://github.com/mazen160/struts-pwn
 echo "Struts-pwn installed!"
 
+echo "Installing Subbrute..."
+sudo git clone https://github.com/TheRook/subbrute
+echo "SubBrute installed!"
+
 echo "Installing Sublist3r"
 git clone https://github.com/aboul3la/Sublist3r
 cd Sublist3r/
@@ -110,44 +162,36 @@ sudo pip install -r requirements.txt
 cd ../
 echo "done"
 
-echo "installing teh_s3_bucketeers"
-git clone https://github.com/tomdev/teh_s3_bucketeers.git
-cd ~/tools/
-echo "done"
+echo "Installing Tomcat-CVE-2017-12617..."
+sudo git clone https://github.com/cyberheartmi9/CVE-2017-12617
+sudo chmod +x CVE-2017-12617/tomcat-cve-2017-12617.py
+echo "Tomcat-CVE-2017-12617 installed!"
 
-echo "installing wpscan"
-git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan*
-gem install bundler && bundle install --without test
-cd ~/tools/
-echo "done"
+echo "Installing TruffleHog..."
+sudo pip install trufflehog
+echo "TruffleHog installed!"
 
-echo "installing dirsearch"
-git clone https://github.com/maurosoria/dirsearch.git
-cd ~/tools/
-echo "done"
+echo "Installing VBScan..."
+sudo git clone https://github.com/rezasp/vbscan
+sudo chmod +x vbscan/*.pl
+echo "VBScan installed!"
 
-echo "installing lazys3"
-git clone https://github.com/nahamsec/lazys3.git
-cd ~/tools/
-echo "done"
+echo "Installing Virtual-Host-Discovery..."
+sudo git clone https://github.com/jobertabma/virtual-host-discovery
+sudo chmod +x virtual-host-discovery/scan.rb
+echo "Virtual-Host-Discovery installed!"
 
-echo "installing virtual host discovery"
-git clone https://github.com/jobertabma/virtual-host-discovery.git
-cd ~/tools/
-echo "done"
+echo "Installing WPScan"
+sudo git clone https://github.com/wpscanteam/wpscan
+cd wpscan/
+sudo gem install bundler && sudo bundle install --without test
+cd ..
+echo "WPScan installed!"
 
-echo "installing sqlmap"
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-cd ~/tools/
-echo "done"
-
-echo "installing knock.py"
-sudo apt-get install python-dnspython
-git clone https://github.com/guelfoweb/knock.git
-cd ~/tools/
-echo "done"
-
+echo "Installing Aquatone..."
+sudo gem install aquatone
+echo "Don't forget to add your API keys!"
+echo "Aquatone installed!"
 
 echo -e "\n\n\n\n\n\n\n\n\n\n\nDone! All tools are set up in /opt"
 ls -la
